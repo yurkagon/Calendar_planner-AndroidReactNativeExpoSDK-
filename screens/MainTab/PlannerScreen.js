e029560e8d14651551bae272e855cead832cc938
yurkagon <owngameplay@gmail.com>
2018-01-11 19:13:20 +0200

    Date picker

--- screens/MainTab/PlannerScreen.js
+++ screens/MainTab/PlannerScreen.js
@@ -1,29 +1,80 @@
 import React from 'react';
+import currentUser from '../../Planner';
 import {
     Text,
     StyleSheet,
     View,
     TextInput,
     TouchableOpacity,
+    DatePickerAndroid,
 } from 'react-native';
 import Colors from '../../constants/Colors';
 import {MaterialIcons} from '@expo/vector-icons';
 
 export default class EventsScreen extends React.Component {
     static navigationOptions = {
-        title: "Create an event"
+        title: "Planner"
     };
+    constructor(props){
+        super(props);
 
+        let startTime = new Date();
+        let endTime = new Date(startTime);
+        endTime.setHours(endTime.getHours()+1);
+
+        this.state={
+            inputText:"",
+            startTime,
+            endTime,
+        }
+    }
+    async setDateAsync(){
+        let date;
+        try {
+            const {action, year, month, day} = await DatePickerAndroid.open({
+                date: new Date(),
+                 // May 25 2020. Month 0 is January.
+                //date: new Date(2020, 4, 25)
+            });
+            if (action !== DatePickerAndroid.dismissedAction) {
+                date = new Date(year,month,day)
+            }
+            else{
+                date = new Date();
+            }
+          } catch ({code, message}) {
+                date = new Date()
+          }
+        return date;
+    }
+    async setStartTimeAsync(){
+        startTime = await this.setDateAsync();
+        this.setState({startTime});
+    }
+    async setEndTimeAsync(){
+        endTime = await this.setDateAsync();
+        this.setState({endTime});
+    }
+    setBaseDates(){
+        let startTime = new Date();
+        let endTime = new Date(startTime);
+        endTime.setHours(endTime.getHours()+1);
+
+        this.setState({
+            startTime,
+            endTime,
+        });
+    }
     render() {
         return (
             <View style={styles.page}>
                 <MaterialIcons
-                        name="event"
-                        size={70}
-                        color="white"
+                    name="event"
+                    size={70}
+                    color="white"
                 />
-                <Field name="TITLE">
-                    <View style={styles.inputField}>
+                <Field name="TITLE" >
+                    <View style={[styles.inputField,{marginBottom:20}]}>
                         <TextInput
                             editable = {true}
                             maxLength = {40}
@@ -35,6 +86,8 @@ export default class EventsScreen extends React.Component {
                             underlineColorAndroid="white"
                             selectionColor={Colors.nowColor}
                             placeholderTextColor="rgba(81, 214, 74,0.7)"
+                            onChangeText={(inputText) => this.setState({inputText})}
+                            value={this.state.inputText}
                         />
                     </View>
                 </Field>
@@ -42,16 +95,35 @@ export default class EventsScreen extends React.Component {
                     <TouchableOpacity
                         style={styles.inputField}
                         activeOpacity={0.7}
-                        //onPress={()=> {}}
-                    />
+                        onPress={this.setStartTimeAsync.bind(this)}
+                    >
+                        <Text style={styles.date}>
+                            {currentUser.formatDateToDisplay(this.state.startTime.toJSON())}
+                        </Text>
+                    </TouchableOpacity>
                 </Field>
                 <Field name="END TIME">
                     <TouchableOpacity
                         style={styles.inputField}
                         activeOpacity={0.7}
-                        //onPress={()=> {}}
-                    />
+                        onPress={this.setEndTimeAsync.bind(this)}
+                    >
+                        <Text style={styles.date}>
+                            {currentUser.formatDateToDisplay(this.state.endTime.toJSON())}
+                        </Text>
+                    </TouchableOpacity>
                 </Field>
+                <TouchableOpacity
+                        style={{marginTop:40}}
+                        activeOpacity={0.7}
+                        //onPress={()=> {}}
+                >
+                    <MaterialIcons
+                        name="add-box"
+                        size={100}
+                        color="white"
+                    />
+                </TouchableOpacity>
 
             </View>
         );
@@ -90,9 +162,18 @@ const styles = StyleSheet.create({
         paddingLeft:5,
         paddingRight: 5,
     },
-    container:{
-        flexDirection: 'row',
-        marginBottom: 30,
+    button:{
+        backgroundColor: 'white',
+        borderRadius: 12,
+        height: 50,
+        width: 50,
+        justifyContent: 'center',
+        paddingRight: 5,
     },
+    date:{
+        color: Colors.nowColor,
+        fontSize: 32,
+        textAlign: "center",
+    }
 
 });
